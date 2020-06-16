2b111bbc6d0946ce53526bd5d0c8ce536734848f
lz05101 <328222051@qq.com>
2020-06-16 15:34:30 +0800

    初始工程

---
+++ XD_Voice.ts
@@ -0,0 +1,22 @@
+/**
+* makecode XD Voice Package.
+* From microbit/micropython Chinese community.
+* http://www.micropython.org.cn
+*/
+
+//% weight=20 color=#0855AA icon="\uf055" block="心点语音"
+namespace XD_Voice{
+    let volume = 30;
+
+    /**
+     * 语音模块初始化串口引脚
+     * @param tx serial tx pin
+     * @param rx serial rx pin
+     */
+    //% blockId="XD_Voice_begin" block="语音模块初始化串口引脚"
+    //% weight=70 blockGap=8
+    //% parts=XD_Vocie trackArgs=0
+    export function begin(tx:SerialPin, rx:SerialPin):void{
+        serial.redirect( tx, rx, 9600);
+    }
+}
