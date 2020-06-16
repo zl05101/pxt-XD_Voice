7625875383faec19a89a2524c6615921a8b95252
lz05101 <328222051@qq.com>
2020-06-16 16:02:10 +0800

    fix

--- XD_Voice.ts
+++ XD_Voice.ts
@@ -8,6 +8,31 @@
 namespace XD_Voice{
     let volume = 30;
 
+    function uartSendData(buf:number[]):void{
+        serial.writeNumbers(buf);
+    }
+
+    function sendPackage(cmd:number, feedBack:number, param:number[], paramLen:number):void{
+        let buf:number[] = [0x7E, 0xFF, 0x0];
+        buf.push(cmd);
+        let dataLen = 4;
+        if (feedBack != 0xFF){
+            buf.push(feedBack);
+            dataLen += 1;
+            for(let i=0; i < paramLen; i++){
+                buf.push(param[i])
+            }
+        } else{
+            for(let i=0; i < paramLen; i++){
+                buf.push(param[i])
+            }
+        }
+        dataLen += paramLen;
+        buf[2] = dataLen - 1;
+        buf.push(0xEF);
+        uartSendData(buf);
+    }
+
     /**
      * 语音模块初始化串口引脚
      * @param tx serial tx pin
@@ -19,4 +44,19 @@ namespace XD_Voice{
     export function begin(tx:SerialPin, rx:SerialPin):void{
         serial.redirect( tx, rx, BaudRate.BaudRate9600);
     }
+
+    /**
+     * 语音模块播放
+     * @param folder 文件夹名
+     * @param file   文件名
+     */
+    //% blockId="XD_Voice_play" block="语音模块播放单个文件"
+    //% weight=70 blockGap=8
+    //% parts=XD_Vocie trackArgs=0
+    export function playByName():void{
+        let na = [1, 1];
+        sendPackage(0xF, 0, na, 2);
+    }
+
 }
+
