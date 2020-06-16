7a068794bad5e003efb1f8bcdf0a76c7c94bae03
lz05101 <328222051@qq.com>
2020-06-16 16:40:06 +0800

    fix

--- XD_Voice.ts
+++ XD_Voice.ts
@@ -7,30 +7,29 @@
 //% weight=20 color=#0855AA icon="\uf055" block="心点语音"
 namespace XD_Voice{
     let volume = 30;
-
-    function uartSendData(buf:number[]):void{
-        serial.writeNumbers(buf);
-    }
+    let w_buf = pins.createBuffer(30);
 
     function sendPackage(cmd:number, feedBack:number, param:number[], paramLen:number):void{
-        let buf:number[] = [0x7E, 0xFF, 0x0];
-        buf.push(cmd);
+        w_buf[0] = 0x7E;
+        w_buf[1] = 0xFF;
+        w_buf[2] = 0x0;
+        w_buf[3] = cmd;
         let dataLen = 4;
         if (feedBack != 0xFF){
-            buf.push(feedBack);
+            w_buf[4] = feedBack;
             dataLen += 1;
             for(let i=0; i < paramLen; i++){
-                buf.push(param[i])
+                w_buf[i+5] = param[i];
             }
         } else{
             for(let i=0; i < paramLen; i++){
-                buf.push(param[i])
+                w_buf[i+4] = param[i];
             }
         }
         dataLen += paramLen;
-        buf[2] = dataLen - 1;
-        buf.push(0xEF);
-        uartSendData(buf);
+        w_buf[2] = dataLen - 1;
+        w_buf[dataLen] = 0xEF;
+        serial.writeBuffer(w_buf);
     }
 
     /**
@@ -53,9 +52,8 @@ namespace XD_Voice{
     //% blockId="XD_Voice_play" block="语音模块播放单个文件"
     //% weight=70 blockGap=8
     //% parts=XD_Vocie trackArgs=0
-    export function playByName():void{
-        let na:number[] = [1, 1];
-        sendPackage(0xF, 0, na, 2);
+    export function play(list:number[]):void{
+        sendPackage(0xF, 0, list, 2);
     }
 
 }
